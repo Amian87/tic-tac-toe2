3368cca5db45459f76913c4a6d62371de3f42e73
Abdulla Mian <miana@claycorp.com>
2019-09-09 18:13:54 -0500

    board tests

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        private List<Tuple<int, string>> Marks = new List<Tuple<int, string>>();
+
+        public List<Tuple<int, string>> CurrentMarks()
+        {
+            return Marks;
+        }
+
+        public void Move(int position)
+        {
+            Marks.Add(Tuple.Create(position, "X"));
+        }
+
+    }
+}
