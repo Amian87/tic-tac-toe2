d176d1a36a99689ed4a395bb6085577c9e899730
Abdulla Mian <miana@claycorp.com>
2019-09-25 11:29:41 -0500

    player can make multiple moves

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class Game
+    {
+        private Board board = new Board();
+        private String currentMark = "X";
+
+        public List<Tuple<int, string>> CurrentMarks()
+        {
+            return board.CurrentMarks();
+        }
+
+        public void Move(int position)
+        {
+            board.Move(position, currentMark);
+            if(currentMark == "X")
+            {
+                currentMark = "O";
+            }
+            else
+            {
+                currentMark = "X";
+            }
+        }
+
+        public string Status()
+        {
+            if (isDraw())
+            {
+                return "Draw";
+            }
+            else if (playerOWins())
+            {
+                return "O";
+            }
+            else if (playerXWins())
+            {
+                return "X";
+            }
+            else
+            {
+                return "In Progress";
+            }
+        }
+
+        private bool playerOWins() 
+        {
+            return board.CurrentMarks().Count == 6;
+        }
+
+        private bool playerXWins()
+        {
+            if (board.CurrentMarks().Contains(Tuple.Create(1, "X")) && board.CurrentMarks().Contains(Tuple.Create(2, "X")) && board.CurrentMarks().Contains(Tuple.Create(3, "X")))
+            {
+                return true;
+            }
+            else if(board.CurrentMarks().Contains(Tuple.Create(4, "X")) && board.CurrentMarks().Contains(Tuple.Create(5, "X")) && board.CurrentMarks().Contains(Tuple.Create(6, "X")))
+            {
+                return true;
+            }
+            else if (board.CurrentMarks().Contains(Tuple.Create(7, "X")) && board.CurrentMarks().Contains(Tuple.Create(8, "X")) && board.CurrentMarks().Contains(Tuple.Create(9, "X")))
+            {
+                return true;
+            }
+
+
+            return board.CurrentMarks().Count >= 7;
+        }
+
+        private bool isDraw()
+        {
+            return board.CurrentMarks().Count == 9;
+        }
+    }
+}
---
+++ TicTacToeTests/GameTests.cs
@@ -0,0 +1,127 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TicTacToe;
+
+namespace TicTacToeTests
+{
+    class GameTests
+    {
+        [Test]
+        public void PlayerOneCanMakeAMoveInANewGame()
+        {
+            Game game = new Game();
+            game.Move(3);
+            Tuple<int, string>[] currentMarks = {Tuple.Create(3, "X")};
+            Assert.AreEqual(game.CurrentMarks(), currentMarks);
+        }
+
+        [Test]
+        public void PlayerTwoCanMakeAMoveInAGame()
+        {
+            Game game = new Game();
+            game.Move(3);
+            game.Move(1);
+            Tuple<int, string>[] currentMarks = {Tuple.Create(3, "X"), Tuple.Create(1, "O")};
+            Assert.AreEqual(game.CurrentMarks(), currentMarks);
+        }
+
+        [Test]
+        public void ThreeMovesInARow()
+        {
+            Game game = new Game();
+            game.Move(2);
+            game.Move(4);
+            game.Move(6);
+            Tuple<int, string>[] currentMarks = {Tuple.Create(2, "X"), Tuple.Create(4, "O"), Tuple.Create(6, "X")};
+            Assert.AreEqual(game.CurrentMarks(), currentMarks);
+        }
+
+        [Test]
+        public void AGameCanHaveAWinnerOfX()
+        {
+            Game game = new Game();
+            game.Move(1);
+            game.Move(2);
+            game.Move(3);
+            game.Move(4);
+            game.Move(5);
+            game.Move(6);
+            game.Move(7);
+            Assert.AreEqual(game.Status(), "X");
+        }
+
+        [Test]
+        public void AGameStartsOffInProgress()
+        {
+            Game game = new Game();
+            Assert.AreEqual(game.Status(), "In Progress");
+        }
+
+        [Test]
+        public void AGameCanEndInADraw()
+        {
+            Game game = new Game();
+            game.Move(1);
+            game.Move(2);
+            game.Move(3);
+            game.Move(4);
+            game.Move(6);
+            game.Move(5);
+            game.Move(7);
+            game.Move(9);
+            game.Move(8);
+            Assert.AreEqual(game.Status(), "Draw");
+        }
+
+        [Test]
+        public void AGameCanHaveAWinnerOfO()
+        {
+            Game game = new Game();
+            game.Move(1);
+            game.Move(4);
+            game.Move(2);
+            game.Move(5);
+            game.Move(7);
+            game.Move(6);
+            Assert.AreEqual(game.Status(), "O");
+        }
+
+        [Test]
+        public void XWinsInTopRow()
+        {
+            Game game = new Game();
+            game.Move(1);
+            game.Move(4);
+            game.Move(2);
+            game.Move(5);
+            game.Move(3);
+            Assert.AreEqual(game.Status(), "X");
+        }
+
+        [Test]
+        public void XWinsMiddleRow()
+        {
+            Game game = new Game();
+            game.Move(4);
+            game.Move(1);
+            game.Move(5);
+            game.Move(7);
+            game.Move(6);
+            Assert.AreEqual(game.Status(), "X");
+        }
+
+        [Test]
+        public void XWinsBottomRow()
+        {
+            Game game = new Game();
+            game.Move(7);
+            game.Move(1);
+            game.Move(8);
+            game.Move(7);
+            game.Move(9);
+            Assert.AreEqual(game.Status(), "X");
+        }
+    }
+}
